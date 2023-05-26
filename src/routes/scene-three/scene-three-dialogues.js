10b5c9fac64fdb07f2d71df86e1bd60bde6a2808
allainerain <allainericci@gmail.com>
2023-05-26 23:06:22 +0800

    edited scene 3 dialogue

--- src/routes/scene-three/scene-three-dialogues.js
+++ src/routes/scene-three/scene-three-dialogues.js
@@ -2,15 +2,15 @@ export const sceneThreeDialogues = {
     sceneName: "three",
     body: [
                 {
-                    speaker: 'Manong Driver',
+                    speaker: 'Manong Driver 1',
                     content: 'Hala, may plano pala talagang i-modernize ang mga jeeps no?'
                 },
                 {
-                    speaker: 'Old Manong Driver 1',
+                    speaker: 'OJ',
                     content: 'Oo nga, sabi kasi nila masama na sa kalusugan at sa kapaligaran kaming mga old jeepneys'
                 },
                 {
-                    speaker: 'Old Manong Driver 1',
+                    speaker: 'Manong Driver 1',
                     content: 'I-pagbabawal na daw nila yung mga jeep na katulad mo. Kaso hindi ko naman kayang bayaran yung ejeep na binebenta nila'
                 },
                 {
@@ -18,16 +18,32 @@ export const sceneThreeDialogues = {
                     content: 'Gusto ko mang makatulong, hindi ko alam pano ka tutulungan. Alam kong bigla-bigla ang pagdedesisyon nito nang walang maayos na pagaaral ng mga epekto nito sa tao.'
                 },
                 {
-                    speaker: 'Old Manong Driver 1',
-                    content: 'Sana nga tumaas pa ang kita natin. Kulang na kasi lagi ang kinikita natin sa pang araw-araw para sa pag papaaral kay Marco at sa gastusin sa bahay. Tignan mo, 8 years na ang dumaan hindi parin kita naa-upgrade. '
+                    speaker: 'Manong Driver 1',
+                    content: 'Oo nga. Kaya nga hindi ko din alam gagawin ko.'
                 },
                 {
-                    speaker: 'OJ',
-                    content: 'Oo nga boss! Marami na rin nagrereklamo na kapwa driver at pasahero kasi maitim na ang usok na lumalabas sa tambutso ko. '
+                    speaker: 'Manong Driver 2',
+                    content: 'Oy pare! Kamusta ka na?'
                 },
                 {
                     speaker: 'Old Manong Driver 1',
-                    content: 'Oo nga. Sana hindi tayo masiraan sa byahe.'
+                    content: 'Heto nag-iisip. Hindi ka na pumapasada?'
+                },
+                {
+                    speaker: 'Manong Driver 2',
+                    content: 'Hindi na. Mabuti nang hindi pumasada kaysa sa laging antayin ang magiging desisyon nila. Si Jipoy ko ginagamit nalang naming pampamilya.'
+                },
+                {
+                    speaker: 'Manong Driver 1',
+                    content: 'e anong pinagkakakitaan mo ngayon?'
+                },
+                {
+                    speaker: 'Manong Driver 2',
+                    content: 'Heto yung binili kong e-trike, si Jipoy. Eto lang ang kaya ng bulsa ko e. Masyadong mahal yung e-jeep nila na yon.'
+                },
+                {
+                    speaker: 'Manong Driver 1',
+                    content: 'oo nga. Nahihirapan na din ako kasi siyempre, nakakasira din tayo sa kapaligiran. Sana matulungan tayo ng gobyerno na makabili o kaya naman ay magbigay sila ng madaling intindihin na mga plano.'
                 }
             ]
 
