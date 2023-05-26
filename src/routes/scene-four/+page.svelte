1bcd48887fc284468a9590d0e3b78d6da26047b4
allainerain <allainericci@gmail.com>
2023-05-26 23:13:44 +0800

    Added tooltips (placeholder)

--- src/routes/scene-four/+page.svelte
+++ src/routes/scene-four/+page.svelte
@@ -1,12 +1,12 @@
 <script>
     import Scene from '../../components/scene.svelte';
-    import { sceneThreeDialogues } from './scene-three-dialogues.js';
-    import { tooltips } from './scene-three-tooltips.js';
+    import { sceneFourDialogues } from './scene-four-dialogues.js';
+    import { tooltips } from './scene-four-tooltips.js';
     
   </script>
   
   <Scene
-    sceneDialogues={sceneThreeDialogues}
+    sceneDialogues={sceneFourDialogues}
     nextScene="scene-four"
     {tooltips}
   />
---
+++ src/routes/scene-four/scene-four-tooltips.js
@@ -0,0 +1,14 @@
+export const tooltips = [
+    { word: 'gasoline', meaning: 'To make modern or up-to-date' },
+    { word: 'energy efficient', meaning: 'Traditional Philippine public utility vehicles' },
+    { word: 'near-zero production', meaning: 'Electronic tricycle'},
+    { word: 'pollutants', meaning: 'Electronic tricycle'},
+    { word: 'nitrogen oxides', meaning: 'Electronic tricycle'},
+    { word: 'particulate matter', meaning: 'Electronic tricycle'},
+    { word: 'carbon dioxide', meaning: 'Electronic tricycle'},
+    { word: 'electricity', meaning: 'Electronic tricycle'},
+    { word: 'renewable resources', meaning: 'Electronic tricycle'},
+    { word: 'solar', meaning: 'Electronic tricycle'},
+    { word: 'wind', meaning: 'Electronic tricycle'},
+    { word: 'hydroelectricity', meaning: 'Electronic tricycle'}
+  ];
