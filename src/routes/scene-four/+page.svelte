cdd378b36d9c56bbc99513ff81685a940f22585a
allainerain <allainericci@gmail.com>
2023-05-26 23:10:49 +0800

    Added scene four dialogue

---
+++ src/routes/scene-four/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Scene from '../../components/scene.svelte';
+    import { sceneThreeDialogues } from './scene-three-dialogues.js';
+    import { tooltips } from './scene-three-tooltips.js';
+    
+  </script>
+  
+  <Scene
+    sceneDialogues={sceneThreeDialogues}
+    nextScene="scene-four"
+    {tooltips}
+  />
+  
+  
