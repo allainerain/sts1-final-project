055fd091a6c19a9952fe1f3002cbb59a017d6bc7
allainerain <allainericci@gmail.com>
2023-05-27 19:05:04 +0800

    added scene three characters (rough)

--- src/routes/scene-three/+page.svelte
+++ src/routes/scene-three/+page.svelte
@@ -2,13 +2,13 @@
     import Scene from '../../components/scene.svelte';
     import { sceneThreeDialogues } from './scene-three-dialogues.js';
     import { tooltips } from './scene-three-tooltips.js';
-    
-  </script>
+    import { sceneCharacters } from './scene-characters.js'
+    import Background from '../../components/background.svelte'
+</script>
   
-  <Scene
-    sceneDialogues={sceneThreeDialogues}
-    nextScene="scene-four"
-    {tooltips}
-  />
+  
+<Background background="scene-one-background.png"/>
+
+<Scene sceneCharacters={sceneCharacters} sceneDialogues={sceneThreeDialogues} nextScene="scene-four" {tooltips} />
   
   
---
+++ src/routes/scene-three/scene-characters.js
@@ -0,0 +1,14 @@
+export const sceneCharacters = {
+  firstCharacter: {
+    name: "OJ",
+    expressions: ['OJ-neutral.png', 'OJ-sad.png', 'OJ-sad.png', 'OJ-sad.png', 'OJ-neutral.png', 'OJ-neutral.png', 'OJ-neutral.png', 'OJ-neutral.png', 'OJ-neutral.png', 'OJ-neutral.png', 'OJ-neutral.png']
+  },
+  secondCharacter: {
+    name: "Gasoline Boy",
+    expressions: ['','','','','jose.png','jose.png','jose.png','jose.png','jose.png','jose.png']
+  },
+  thirdCharacter: {
+    name: "Manuel",
+    expressions: ['manuel-shocked.png', 'manuel-neutral.png', 'manuel-sad.png', 'manuel-sad.png', 'manuel-sad.png', 'manuel-neutral.png', 'manuel-neutral.png', 'manuel-neutral.png', 'manuel-neutral.png', 'manuel-neutral.png', 'manuel-neutral.png']
+  }
+};
