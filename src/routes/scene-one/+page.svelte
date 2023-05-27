a9f322880870803e92cb474d8789acf4b7c4c0a3
allainerain <allainericci@gmail.com>
2023-05-27 17:16:59 +0800

    Added background to scene one

--- src/routes/scene-one/+page.svelte
+++ src/routes/scene-one/+page.svelte
@@ -4,5 +4,32 @@
     import { sceneCharacters } from './scene-characters.js'
 </script>
 
-<Scene sceneCharacters = {sceneCharacters} sceneDialogues={sceneOneDialogues} nextScene='/scene-two' />
+<div class="background">
+    <img src='/backgrounds/scene-one-background.png' alt='scene one background' />
+</div>
 
+<Scene sceneCharacters={sceneCharacters} sceneDialogues={sceneOneDialogues} nextScene='/scene-two' />
+
+<style>
+    .background {
+        width: 100%;
+        height: 70vh;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        background-position: center top; /* Adjust the position here */
+    }
+
+    @media (max-width: 1000px) {
+      .background {
+        height: 75vh;
+      }
+    }
+
+    .background img {
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+    }
+</style>
