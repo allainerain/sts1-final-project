10c1ec67d37a8597921e51929149944d5bc56f70
allainerain <allainericci@gmail.com>
2023-05-27 17:21:00 +0800

    Made resusable background component

---
+++ src/components/background.svelte
@@ -0,0 +1,42 @@
+<script>
+    export let background;
+
+    import { afterUpdate } from 'svelte';
+  
+    let backgroundSrc;
+  
+    afterUpdate(() => {
+        backgroundSrc = `/backgrounds/${background}`;
+    });
+
+
+
+</script>
+
+<div class="background">
+    <img src={backgroundSrc} alt='scene one background' />
+</div>
+
+<style>
+    .background {
+        width: 100%;
+        height: 70vh;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        background-position: center top; /* Adjust the position here */
+    }
+
+    @media (max-width: 1000px) {
+      .background {
+        height: 75vh;
+      }
+    }
+
+    .background img {
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+    }
+</style>
--- src/routes/scene-one/+page.svelte
+++ src/routes/scene-one/+page.svelte
@@ -2,34 +2,10 @@
     import { sceneOneDialogues } from './scene-one-dialogues.js';
     import Scene from '../../components/scene.svelte'
     import { sceneCharacters } from './scene-characters.js'
+    import Background from '../../components/background.svelte'
 </script>
 
-<div class="background">
-    <img src='/backgrounds/scene-one-background.png' alt='scene one background' />
-</div>
-
+<Background background="scene-one-background.png"/>
 <Scene sceneCharacters={sceneCharacters} sceneDialogues={sceneOneDialogues} nextScene='/scene-two' />
 
-<style>
-    .background {
-        width: 100%;
-        height: 70vh;
-        position: fixed;
-        top: 0;
-        left: 0;
-        z-index: -1;
-        background-position: center top; /* Adjust the position here */
-    }
 
-    @media (max-width: 1000px) {
-      .background {
-        height: 75vh;
-      }
-    }
-
-    .background img {
-        object-fit: cover;
-        width: 100%;
-        height: 100%;
-    }
-</style>
