c54cc9a2b01ad7261d99b63883e9cf6cc2fcbb63
allainerain <allainericci@gmail.com>
2023-05-28 13:52:44 +0800

    Updated crossword font

--- src/components/crosswordPuzzle.svelte
+++ src/components/crosswordPuzzle.svelte
@@ -1,11 +1,23 @@
 <script>
     import Crossword from "svelte-crossword";
     import crosswordData from "./crosswordData.json"
-  
+
 </script>
-  
-<Crossword class='svelte-crossword' data="{crosswordData}" />
+
+<div >
+
+    <Crossword  data="{crosswordData}" />
+
+</div>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Pangolin&display=swap');
+
+div {
+    --xd-font: 'Pangolin';
+    max-width: 960px;
+    margin: 3rem auto;
+    margin-bottom: 6rem;
+  }
 
 </style>
