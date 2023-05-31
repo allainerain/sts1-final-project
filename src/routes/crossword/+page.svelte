5d5f86d62aa06de3702c8b30986abdc4c4a7e583
allainerain <allainericci@gmail.com>
2023-06-01 00:23:56 +0800

    Updated crossword

--- src/routes/crossword/+page.svelte
+++ src/routes/crossword/+page.svelte
@@ -3,24 +3,25 @@
     import Background from '../../components/background.svelte'
     // import '/backgrounds/crossword/-background.jpg'
 
+    // window.onload = function() {
+    //     location.reload();
+    // };
 </script>
 
 <body>
     <CrosswordPuzzle class='crossword-puzzle'/>
 </body>
 
-
-
 <style>
-
-body{
-    top:0;
-    bottom:0;
-    width:100;
-    height:100;
-    margin:0 0 0 0;
-    background-image: url('/backgrounds/crossword-background.jpg')
+body {
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    background-image: url('/backgrounds/crossword-background.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
 }
-
 </style>
-
