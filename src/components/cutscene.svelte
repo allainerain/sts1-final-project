ac69e2127beb86c4adecd0500b3579f96e2e841b
allainerain <allainericci@gmail.com>
2023-06-01 22:12:34 +0800

    Added cutscene component

---
+++ src/components/cutscene.svelte
@@ -0,0 +1,83 @@
+<script>
+    export let image;
+    export let text;
+    export let link;
+
+    import { afterUpdate } from 'svelte';
+
+    let imageSrc;
+
+    afterUpdate(() => {
+        imageSrc = `/backgrounds/${image}`;
+    });
+
+</script>
+
+<section class="card">
+    <div class="image-wrapper">
+        <img src={imageSrc} alt={imageSrc}/>
+    </div>
+    <div class="button-wrapper">
+        <a href={link}>
+            <button>{text}</button>
+        </a>
+    </div>
+</section>
+
+<style>
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+
+    .card {
+        position: relative;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        background-color: black;
+    }
+
+    .image-wrapper {
+        flex-grow: 1;
+    }
+
+    .button-wrapper {
+        background-color: black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: fit-content;
+        padding: 10px;
+    }
+
+    button {
+        font-family: 'Pangolin', cursive;
+        font-size: x-large;
+        background-color: rgb(239, 239, 239);
+        box-shadow: none;
+        border: 0px;
+        border-radius: 5px;
+        padding: 10px 15px;
+        margin: 10px;
+        cursor: pointer;
+        width: 200px;
+    }
+
+    @media (min-width: 1500px) {
+        .card {
+            max-width: 82vw;
+            margin: 0 8vw;
+        }
+
+    }
+
+    @media (min-width: 1700px) {
+        .card {
+            max-width: 80vw;
+            margin: 0 10vw;
+        }
+
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,28 @@
 <script>
     import { fade } from 'svelte/transition'
+    import Cutscene from '../components/cutscene.svelte'
 </script>
 
 <div transition:fade="{{ duration: 1000}}" class = "main">
-    <h1 class = "title"> Story Title </h1>
+    <!-- <h1 class = "title"> Story Title </h1>
     <p class = "description"> Descripton something hello this is the group project by group 12 for our STS final project under Minimo Likha.</p>
     <a href="/scene-one">
         <button>Laro na!</button>
-    </a>
+    </a> -->
+    <Cutscene image="scene-one-cutscene.png" text="Laro na!" link="/scene-one"/>
 </div>
 
 <style>
 
-.main{
+/* .main{
     margin: 10%;
     padding: 10%;
     background-color: lightblue;
     text-align: center;
-}
+} */
 
-.title{
+/* .title{
     text-align: center;
-}
+} */
 
 </style>
