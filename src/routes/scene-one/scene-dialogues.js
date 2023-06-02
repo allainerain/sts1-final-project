2893019ee51c8257c2c73b7e21e0e4306f02c1c2
allainerain <allainericci@gmail.com>
2023-06-03 00:56:30 +0800

    Changed names and scene one dialogues file

--- src/routes/scene-one/scene-dialogues.js
+++ src/routes/scene-one/scene-dialogues.js
@@ -2,22 +2,18 @@ export const sceneDialogues = {
     sceneName: "one",
     body: [
                 {
-                    image: 'manong-driver-happy.png',
-                    speaker: 'Young Manong Driver',
+                    speaker: 'Young Manuel',
                     content: 'Ayan busog ka na ulit, handa ka na para bukas. Full-tank yan, may sobra pa tayo!'
                 },
                 {
-                    image: 'manong-driver-happy.png',
                     speaker: 'Young OJ',
                     content: 'Ayos bossing! Madami tayong kita ngayon a. Makakabili ka pa ng 5 kilong bigas at may matitira pang pambili ng karne para sa panghapunan.'
                 },
                 {
-                    image: 'manong-driver-happy.png',
-                    speaker: 'Young Manong Driver',
+                    speaker: 'Young Manuel',
                     content: 'Oo nga! Salamat sa tulong mo lagi. Kapag nagtuloy-tuloy yan, mapapatapos natin si Marco. Hindi lang yan, maup-upgrade pa kita sa katapusan ng taon!'
                 },
                 {
-                    image: 'manong-driver-happy.png',
                     speaker: 'Young OJ',
                     content: 'Gusto ko yan!'
                 }
